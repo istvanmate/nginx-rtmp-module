ef87a606dd900f5985865760a9b8a7374ed0de27
Fahnenfluchtige <kr4snovalya@gmail.com>
2025-02-05 21:38:06 +0300

    Little fix

--- ngx_rtmp_relay_module.c
+++ ngx_rtmp_relay_module.c
@@ -1293,8 +1293,8 @@ ngx_rtmp_relay_on_status(ngx_rtmp_session_t *s, ngx_rtmp_header_t *h,
                              sizeof(in_elts_meta) / sizeof(in_elts_meta[0])) != NGX_OK)
         {
             ngx_log_error(NGX_LOG_ERR, s->connection->log, 0,
-                      "relay: failed to parse AMF message");
-                      
+                      "relay: failed to parse AMF metadata");
+
             return NGX_ERROR;
         }
     } else {
@@ -1302,7 +1302,7 @@ ngx_rtmp_relay_on_status(ngx_rtmp_session_t *s, ngx_rtmp_header_t *h,
                                  sizeof(in_elts) / sizeof(in_elts[0])) != NGX_OK)
         {
             ngx_log_error(NGX_LOG_ERR, s->connection->log, 0,
-                      "relay: failed to parse AMF metadata");
+                      "relay: failed to parse AMF message");
 
             return NGX_ERROR;
         }
